a4b3e63ad15df1ccef393de2d41d2e85f6436a34
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-05-24 21:52:33 +0530

    create layout

--- src/components/ChatSection/ChatSection.jsx
+++ src/components/ChatSection/ChatSection.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./ChatSection.css"
 import Darkmode from '../Darkmode/Darkmode'
+import { LuSendHorizontal } from "react-icons/lu";
 
 function ChatSection() {
   return (
     <div className='chatsection'>
-      <Darkmode/>
+      <div className="topsection">
+        <div className="headings">
+          <span>HELLO MINKU,</span>
+          <span>I'm Your Own Assistant</span>
+          <span>What can I help you?</span>
+        </div>
+      </div>
+      <div className="bottomsection">
+        <input type="text" placeholder='Enter a prompt'/>
+        <button id='sendbtn'><LuSendHorizontal /></button>
+        <Darkmode/>
+      </div>
     </div>
   )
 }
